7801ba8e7b3b52e7fd3c7200055c18b34184e549
chocobo555 <ian.harris1@loop.colum.edu>
2015-02-24 18:50:57 -0500

    Oculus Rotation
    Added and changed scripts to get the Oculus rift to rotate the players
    body.

--- Assets/Scripts/TurnWithRift.cs
+++ Assets/Scripts/TurnWithRift.cs
@@ -7,6 +7,10 @@ public class TurnWithRift : MonoBehaviour
 
 	public GameObject body;
 
+	bool enteredRadiusRight = false;
+	bool enteredRadiusLeft = false;
+
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,12 +23,40 @@ public class TurnWithRift : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		//print("non-local" + transform.rotation.eulerAngles.y);
+		//print(transform.localRotation.eulerAngles.y);
 
-		if (transform.rotation.eulerAngles.y > 45 && transform.rotation.eulerAngles.y < 315) 
+		if (transform.localRotation.eulerAngles.y > 45 && transform.localRotation.eulerAngles.y < 50) 
 		{
-			print("reached turning angle");
-			body.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+
+			enteredRadiusRight = true;
+			enteredRadiusLeft = false;
+
 		}
+
+		if(transform.localRotation.eulerAngles.y < 315 && transform.localRotation.eulerAngles.y > 310)
+		{
+		
+			enteredRadiusLeft = true;
+			enteredRadiusRight = false;
+
+		}
+
+		if(transform.localRotation.eulerAngles.y > 45 && transform.localRotation.eulerAngles.y < 315) 
+		{
+			//print("angle reached");
+
+			if (enteredRadiusRight == true) 
+			{
+				print ("over 45");
+				body.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+			}
+			else if(enteredRadiusLeft == true)
+			{
+				print ("under 315");
+				body.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+			}
+
+		}
+
 	}
 }
---
+++ Assets/Scripts/VisionCone.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionCone : MonoBehaviour 
+{
+	public GameObject vision;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		//vision = GameObject.Find("LeftEyeAnchor");
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*
+		Quaternion myTempRot = GetComponent<RectTransform>().localRotation;// .rotation;
+		
+		//myTempRot.z = vision.transform.localRotation.eulerAngles.y;
+		myTempRot.z = vision.transform.rotation.y;
+		
+		myTempRot.z *= -1;
+
+		myTempRot.z = Mathf.Clamp(myTempRot.z, -45, 45); 
+
+
+		Mathf.Clamp(myTempRot.z, -315, 45);
+
+		GetComponent<RectTransform>().localRotation = myTempRot;
+		*/
+
+
+
+
+		if (transform.rotation.y > 45 && transform.rotation.y < 315) 
+		{
+			
+			transform.Rotate(0, 0, vision.GetComponent<TurnWithRift>().turnSpeed * Time.deltaTime);
+
+		}
+
+		
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
