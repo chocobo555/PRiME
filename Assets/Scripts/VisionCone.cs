cb20901d39608405469ffd2f805e34edcd333bf4
chocobo555 <ian.harris1@loop.colum.edu>
2015-03-06 14:32:19 -0600

    still trying to clamp made some headway though

--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -17,30 +17,31 @@ public class VisionCone : MonoBehaviour
 	void Update () 
 	{
 
-		Quaternion myTempRot = GetComponent<RectTransform>().localRotation;// .rotation;
-		
+		Quaternion myTempRot = Quaternion.identity;
+
+		myTempRot.eulerAngles = GetComponent<RectTransform>().eulerAngles; // localRotation;// .rotation;
+
 		//myTempRot.z = vision.transform.localRotation.eulerAngles.y;
-		myTempRot.z = vision.transform.rotation.y;
+		myTempRot.z = vision.transform.eulerAngles.y;
 		
 		myTempRot.z *= -1;
 
 
-		myTempRot.z = Mathf.Clamp(myTempRot.z, -45, 45); 
+		//myTempRot.z = Mathf.Clamp(myTempRot.z, -45, 45); 
 		//Mathf.Clamp(myTempRot.z, 0, 45);
 		print(myTempRot.eulerAngles.z);
 
 
-		//GetComponent<RectTransform>().rotation = myTempRot;
+		GetComponent<RectTransform>().eulerAngles = myTempRot.eulerAngles;
 
 
 		/*
 		if (transform.rotation.y > 45 && transform.rotation.y < 315) 
 		{
-			
 			transform.Rotate(0, 0, vision.GetComponent<TurnWithRift>().turnSpeed * Time.deltaTime);
-
 		}
 		*/
+
 	}
 
 
