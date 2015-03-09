33fe9d3fcc959cc37135fa22c37fbc3a9dca6d42
chocobo555 <ian.harris1@loop.colum.edu>
2015-03-09 00:18:30 -0400

    added raycast for lock-on

---
+++ Assets/Scripts/LockOn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LockOn : MonoBehaviour 
+{
+
+	public KeyCode scan = KeyCode.E;
+	GameObject scannerMonitor1;
+	GameObject scannerMonitor2;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		scannerMonitor1 = GameObject.Find ("Scanner Monitor1");
+		scannerMonitor2 = GameObject.Find ("Scanner Monitor2");
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKeyDown(scan)) 
+		{
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			RaycastHit hit;
+			Physics.Raycast(transform.position, fwd, out hit, 10); 
+			if (hit.collider != null) 
+			{
+				scannerMonitor1.GetComponent<Text>().text = hit.collider.gameObject.name;
+				scannerMonitor2.GetComponent<Text>().text = hit.collider.gameObject.name;
+
+				print(hit.collider.gameObject.name);
+			}
+		}
+	
+
+	}
+
+
+}
--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -12,76 +12,25 @@ public class VisionCone : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		//vision = GameObject.Find("LeftEyeAnchor");
+	
 	}
 
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
-		/*
-		Quaternion myTempRot = Quaternion.identity;
-		myTempRot.eulerAngles = GetComponent<RectTransform>().eulerAngles; // localRotation;// .rotation;
-
-		//myTempRot.z = vision.transform.localRotation.eulerAngles.y;
-		myTempRot.z = vision.transform.eulerAngles.y;
-		myTempRot.z *= -1;
-
-
-		//myTempRot.z = Mathf.Clamp(myTempRot.z, -45, 45); 
-		//Mathf.Clamp(myTempRot.z, 0, 45);
-		print(myTempRot.eulerAngles.z);
-
-		GetComponent<RectTransform>().eulerAngles = myTempRot.eulerAngles;
-		*/
-
-
-
-		//Quaternion rotation;
-
-		//Vector3 myTempPos = vision.transform.position;
-
-		//myTempPos.z = vision.transform.position.y;
-
-		//rotation = Quaternion.LookRotation(myTempPos);
 
 		tempRot = transform.rotation;
 
 		tempRot.z = vision.transform.rotation.y;
-		tempRot.z *= -1;
+		tempRot.z = Mathf.Clamp(tempRot.z, -45, 45);
 
+		tempRot.z *= -1;
 
 		transform.rotation = Quaternion.Slerp(transform.rotation, tempRot, rotSpeed * Time.deltaTime); 
 
-
-		/*
-		Vector3 pos = transform.position - target.transform.position;
-		pos.x = transform.position.x;
-		rotation = Quaternion.LookRotation(pos);
-		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, rotSpeed * Time.deltaTime);
-		*/
 	}
 
-
-	void LateUpdate()
-	{
-
-		float clampedZ = 0;
-
-		if (transform.eulerAngles.z < 180) 
-		{
-			clampedZ = Mathf.Clamp(transform.eulerAngles.z , 0f, 45f);
-		}
-		else 
-		{
-			clampedZ = Mathf.Clamp(transform.eulerAngles.z , 315f, 359f);
-		}
-
-
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y , clampedZ); 
-	}
-	
 
 }
 
