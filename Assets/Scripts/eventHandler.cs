8aa093c2604c94251be0a279cc514c965d3fce43
chocobo555 <ian.harris1@loop.colum.edu>
2015-03-10 21:14:06 -0500

    event handler practice

---
+++ Assets/Scripts/eventHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eventHandler : MonoBehaviour 
+{
+
+	public delegate void powerupHandler(bool isPowered);
+	
+	public static event powerupHandler powerUpHandle;
+
+	
+	
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(5, 5, 150, 40), "HI!")) 
+		{
+			if (powerUpHandle != null) 
+			{
+				powerUpHandle(true);
+			}
+
+		}
+
+		if (GUI.Button(new Rect(5, 50, 150, 40), "HI!")) 
+		{
+			if (powerUpHandle != null) 
+			{
+				powerUpHandle(false);
+			}
+			
+		}
+
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+
+}
---
+++ Assets/Scripts/eventListener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eventListener : MonoBehaviour 
+{
+
+
+	void OnEnable()
+	{
+		eventHandler.powerUpHandle += OnPoweredUp;
+	}
+
+
+	void OnDisable()
+	{
+		eventHandler.powerUpHandle -= OnPoweredUp;
+	}
+
+
+	void OnPoweredUp(bool isPowered)
+	{
+		Debug.Log(isPowered);
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+}
