eee755c5e9299e35c745fad843b9c91da8deac7f
chocobo555 <ian.harris1@loop.colum.edu>
2015-03-17 19:10:09 -0400

    polyworld terrain added

--- Assets/Scripts/TurnWithRift2.cs
+++ Assets/Scripts/TurnWithRift2.cs
@@ -5,6 +5,10 @@ public class TurnWithRift2 : MonoBehaviour
 {
 	//public float turnSpeed = 60f;
 	public float firstDegree = 5f;
+	public float secondDegree = 10f;
+	public float thirdDegree = 15f;
+	public float fourthDegree = 20f;
+	public float fifthDegree = 25f;
 	
 	public GameObject body;
 	
@@ -52,17 +56,55 @@ public class TurnWithRift2 : MonoBehaviour
 					body.transform.Rotate(0, firstDegree * Time.deltaTime, 0);
 				}
 				 
-				if (transform.localRotation.eulerAngles.y <= 5) 
+				if (transform.localRotation.eulerAngles.y > 25 && transform.localRotation.eulerAngles.y <= 45) 
 				{
+					body.transform.Rotate(0, secondDegree * Time.deltaTime, 0);
+				}
 
+				if (transform.localRotation.eulerAngles.y > 45 && transform.localRotation.eulerAngles.y <= 65) 
+				{
+					body.transform.Rotate(0, thirdDegree * Time.deltaTime, 0);
+				}
+
+				if (transform.localRotation.eulerAngles.y > 65 && transform.localRotation.eulerAngles.y <= 85) 
+				{
+					body.transform.Rotate(0, fourthDegree * Time.deltaTime, 0);
+				}
+				
+				if (transform.localRotation.eulerAngles.y > 85 && transform.localRotation.eulerAngles.y <= 105) 
+				{
+					body.transform.Rotate(0, fifthDegree * Time.deltaTime, 0);
 				}
 	
 			}
+
 			else if(enteredRadiusLeft == true)
 			{
-				//	print ("under 315");
-				body.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
 
+				if (transform.localRotation.eulerAngles.y <= 355 && transform.localRotation.eulerAngles.y >= 335) 
+				{
+					body.transform.Rotate(0, -firstDegree * Time.deltaTime, 0);
+				}
+				
+				if (transform.localRotation.eulerAngles.y < 335 && transform.localRotation.eulerAngles.y >= 315) 
+				{
+					body.transform.Rotate(0, -secondDegree * Time.deltaTime, 0);
+				}
+				
+				if (transform.localRotation.eulerAngles.y < 315 && transform.localRotation.eulerAngles.y >= 295) 
+				{
+					body.transform.Rotate(0, -thirdDegree * Time.deltaTime, 0);
+				}
+				
+				if (transform.localRotation.eulerAngles.y < 295 && transform.localRotation.eulerAngles.y >= 275) 
+				{
+					body.transform.Rotate(0, -fourthDegree * Time.deltaTime, 0);
+				}
+
+				if (transform.localRotation.eulerAngles.y < 275 && transform.localRotation.eulerAngles.y >= 255) 
+				{
+					body.transform.Rotate(0, -fifthDegree * Time.deltaTime, 0);
+				}
 			}
 			
 		}
