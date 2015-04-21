89df6335d809dab34d36074213bc7f9479c3a713
chocobo555 <ian.harris1@loop.colum.edu>
2015-04-21 15:38:18 -0500

    UI Wrapper done

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,9 +6,12 @@ public class UIManager : MonoBehaviour
 	public GUISkin myGuiSkin;
 	public Texture2D background, logo;
 
-	private Rect windowRect = new Rect((Screen.width / 2) - 100, Screen.height / 2, 200, 200);
+	private Rect windowRect = new Rect((Screen.width / 2) - 250, (Screen.height / 2) - 250, 400, 500);
 
 	private string menuState;
+
+	public GUIStyle mainStyle;
+	public GUIStyle buttonStyle;
 
 	private string main = "main";
 	private string options = "options";
@@ -39,42 +42,42 @@ public class UIManager : MonoBehaviour
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
 
 		if (logo != null) 
-			GUI.DrawTexture(new Rect(Screen.width / 2 -100, 30, 200, 200), logo);
+			GUI.DrawTexture(new Rect(Screen.width / 2 -300, 30, 600, 200), logo);
 
 		GUI.skin = myGuiSkin;
 
 		if (menuState == main) 
 		{
-			windowRect = GUI.Window(0, windowRect, MenuFunc, "Main Menu");
+			windowRect = GUI.Window(0, windowRect, MenuFunc, "Main Menu", mainStyle);
 		}
 
 		if (menuState == options) 
 		{
-			windowRect = GUI.Window(1, windowRect, OptionsFunc, "Options");
+			windowRect = GUI.Window(1, windowRect, OptionsFunc, "Options", mainStyle);
 		}
 
 		if (menuState == credits)
 		{
-			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), textToDisplay);
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), textToDisplay, mainStyle);
 		}
 	}
 
 
 	void MenuFunc(int id)
 	{
-		if (GUILayout.Button("Play Game"))
+		if (GUILayout.Button("Play Game", buttonStyle))
 		{
-			Application.LoadLevel("level1");
+			Application.LoadLevel("scene1");
 		}
-		if (GUILayout.Button("Options")) 
+		if (GUILayout.Button("Options", buttonStyle)) 
 		{
 			menuState = options;
 		}
-		if (GUILayout.Button("Credits"))
+		if (GUILayout.Button("Credits", buttonStyle))
 		{
 			menuState = credits;
 		}
-		if (GUILayout.Button("Quit Game"))
+		if (GUILayout.Button("Quit Game", buttonStyle))
 		{
 			Application.Quit();
 		}
@@ -83,10 +86,10 @@ public class UIManager : MonoBehaviour
 
 	void OptionsFunc(int id)
 	{
-		GUILayout.Box("Volume");
+		GUILayout.Box("Volume", buttonStyle);
 		volume = GUILayout.HorizontalSlider(volume, 0.0f, 1.0f);
 		AudioListener.volume = volume;
-		if (GUILayout.Button("Back")) 
+		if (GUILayout.Button("Back", buttonStyle)) 
 			menuState = main;
 	}
 
