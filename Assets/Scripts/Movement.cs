1b94778b511b4637c0041343e561551fe998da28
chocobo555 <ian.harris1@T3500-Oblivion.iam.local>
2015-04-22 22:30:26 -0500

    fixed new sprint move stuff

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 public class Movement : MonoBehaviour 
 {
 	private float groundSpeed = 3.0f;
-	public float airSpeed = 3.0f;
-	public float airDrift = 1f;
+	private float airSpeed = 3.0f;
+	private float airDrift = 1f;
 
-	public float jumpHeight = 7.0f;
-	public float jumpLength = .1f;
-	public float jumpForce = 1f;
+	private float jumpHeight = 11f;
+	private float jumpLength = 1.3f;
+
+	private float jumpForce = 1f;
+	private float sprintForce = 5;
+
 	GameObject JumpForceSlider1;
-	GameObject jumpForceSlider2;
+	GameObject JumpForceSlider2;
+	GameObject SprintForceSlider1;
+	GameObject SprintForceSlider2;
 
 	public float gravity = 20.0f;
 	public float rotationSpeed;
@@ -26,11 +31,15 @@ public class Movement : MonoBehaviour
 
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		myController = GetComponent<CharacterController>();
+
 		JumpForceSlider1 = GameObject.Find("JumpForceSlider1");
-		jumpForceSlider2 = GameObject.Find("JumpForceSlider2");
+		JumpForceSlider2 = GameObject.Find("JumpForceSlider2");
+		SprintForceSlider1 = GameObject.Find("SprintForceSlider1");
+		SprintForceSlider2 = GameObject.Find("SprintForceSlider2");
+
 		AM = GameObject.Find("CenterEyeAnchor").GetComponent<AudioMaster>();
 
 	}
@@ -39,11 +48,20 @@ public class Movement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		/*
-		float turn = Input.GetAxis("Horizontal");
-		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-		*/
 
+		if (Input.GetKeyDown(KeyCode.Keypad3))
+			jumpForce++;
+
+		if (Input.GetKeyDown(KeyCode.Keypad1)) 
+			jumpForce--;
+
+		if (jumpForce < 1) 
+			jumpForce = 1;
+	
+		if (jumpForce > 4) 
+			jumpForce = 4;
+
+		/*
 		float scrollDirection = Input.GetAxis ("Mouse ScrollWheel");
 		if (scrollDirection > 0) 
 		{
@@ -61,13 +79,14 @@ public class Movement : MonoBehaviour
 		{
 			jumpForce = 6;
 		}
+		*/
 
 		JumpForceSlider1.GetComponent<Slider>().value = jumpForce;
-		jumpForceSlider2.GetComponent<Slider>().value = jumpForce;
+		JumpForceSlider2.GetComponent<Slider>().value = jumpForce;
 		//print(jumpForce);
 
-		float TempJumpHeight = jumpHeight * jumpForce;
-		float TempJumpLength = jumpLength * jumpForce;
+		float TempJumpHeight = jumpHeight * (jumpForce / 2);
+		float TempJumpLength = jumpLength * (jumpForce / 2);
 
 
 		if(myController.isGrounded == true) 
@@ -85,26 +104,36 @@ public class Movement : MonoBehaviour
 			//current
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
+		
+
+			if(Input.GetKeyDown(KeyCode.Keypad6))
+				sprintForce = sprintForce + 5;
+
+			if(Input.GetKeyDown(KeyCode.Keypad4))
+				sprintForce = sprintForce - 5;
+
+			if(sprintForce < 5)
+				sprintForce = 5;
+
+			if(sprintForce > 30)
+				sprintForce = 30;
 
 
+			SprintForceSlider1.GetComponent<Slider>().value = sprintForce;
+			SprintForceSlider2.GetComponent<Slider>().value = sprintForce;
 
 
-			Vector3 velocity = new Vector3(1, 0, 0);
-			float tempGroundSpeed;
-			if(myController.velocity.z > velocity.z)
-			{
-				tempGroundSpeed = groundSpeed * 1.1F;
-			}
+			if(groundSpeed > sprintForce)
+				groundSpeed = sprintForce;
+
+			Vector3 myVelocity = new Vector3(1, 0, 0);
+
+			if(myController.velocity.z != 0 || myController.velocity.x != 0)
+				groundSpeed *= 1.03F;
 			else 
-			{
-				tempGroundSpeed = groundSpeed;
-			}
-			print(tempGroundSpeed);
+				groundSpeed = 3;
 
 			moveDirection *= groundSpeed;
-
-
-
 
 
 			AM.audioSource.Stop();
@@ -112,31 +141,24 @@ public class Movement : MonoBehaviour
 			if(Input.GetButton("Jump")) 
 			{
 				moveDirection *= TempJumpLength;
-				moveDirection.y = TempJumpHeight;
-
+				moveDirection.y = TempJumpHeight + (moveDirection.magnitude / 1.5F);
 
 				// *****************possible fix
 				Vector3 tempvec = new Vector3();
 				tempvec = Vector3.ClampMagnitude(tempvec, 10f);
 				// ****************************
 
-				//AM.JumpSound();
-
-				print(jumpHeight);
-			
-
+				AM.JumpSound();
 			}
 		}
 
 		else 
 		{
-	
 			// We are not grounded. We can still influence the movement with the horizontal and vertical axis, but not so strong.
 			Vector3 tempVec = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * airDrift, 0, Input.GetAxis("Vertical") * Time.deltaTime * airSpeed);
 			moveDirection += transform.TransformDirection(tempVec);
 
 			//moveDirection += transform.TransformDirection(Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * airDrift, 0, Input.GetAxis("Vertical") * Time.deltaTime * airSpeed));
-
 		}
 
 		moveDirection.y -= gravity * Time.deltaTime;
