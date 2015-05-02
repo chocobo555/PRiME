08a1040fc0a221ffeb6fb341f835a3825c8fefb2
chocobo555 <ian.harris1@loop.colum.edu>
2015-05-02 15:51:24 -0500

    camera/gui stuff

---
+++ Assets/Scripts/FaceCharacter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCharacter : MonoBehaviour 
+{
+	GameObject centerAnchor;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		centerAnchor = GameObject.Find("CenterEyeAnchor");
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(centerAnchor.transform.position, -Vector3.up); 
+	}
+}
